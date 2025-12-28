0fd5c34503e20414f709ae18ce6385ff3087ec1e
Bruno <santosb17.bs@gmail.com>
2025-12-28 08:31:27 -0300

    first commit project healthtrack

---
+++ src/test/java/com/brunovirsan/healthtrack/HealthtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunovirsan.healthtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
